5006e14bba055c5cb49bce6f4fb68f11e1853522
JTShocks <jacobdreyer2002@gmail.com>
2024-02-08 21:50:43 -0500

    Initial player script

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    // Player has the camera and moves around
+    // Need to get ref to the CharacterController
+
+    CharacterController playerController;
+
+    // What does the player need to be able to do?
+    //1. Move around
+    //2. Crouch (lower the camera and reduce the player hitbox)
+    //3. Interact with objects (use an outsider Interactor component)
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
