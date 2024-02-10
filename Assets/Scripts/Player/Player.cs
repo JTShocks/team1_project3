cee1d0cedcda93b3254da609965dcc907f435164
JTShocks <jacobdreyer2002@gmail.com>
2024-02-10 12:43:27 -0500

    Working on player crouch

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,19 +10,25 @@ public class Player : MonoBehaviour
     // Need to get ref to the CharacterController
 
     CharacterController playerController;
+    Camera playerCamera;
 
     Vector3 movementDirection;
     Vector2 inputDirection;
     public float playerMoveSpeed = 8f;
 
+    //Interactor interactor
+
     // What does the player need to be able to do?
     //1. Move around
     //2. Crouch (lower the camera and reduce the player hitbox)
     //3. Interact with objects (use an outsider Interactor component)
+    //4. Pickup objects, then throw them in a direction
+    
     // Start is called before the first frame update
     void Start()
     {
         playerController = GetComponent<CharacterController>();
+        //interactor = GetComponent<Interactor>();
     }
 
     // Update is called once per frame
@@ -37,4 +43,9 @@ public class Player : MonoBehaviour
         inputDirection = value.Get<Vector2>();
         
     }
+
+     void OnCrouch()
+     {
+        //The camera should lower down to .6 (a little higher than the chest)
+     }
 }
