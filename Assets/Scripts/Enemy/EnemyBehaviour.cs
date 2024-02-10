f7e297baf73b0f756e6c67aba13d0b9b773da3eb
JTShocks <jacobdreyer2002@gmail.com>
2024-02-09 20:12:26 -0500

    Minor tweaks

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+
+
+    //What is in the enemy behaviour?
+    //STATES
+    //Idle = It is standing still
+    //Patrolling = It is moving along it's waypoints
+    //Alert = IF it sees the player, but doesn't fill it's awareness, the enemy will move to where it last noticed the player
+    //Chase =  It will move towards the player and follow them
     // Start is called before the first frame update
     void Start()
     {
