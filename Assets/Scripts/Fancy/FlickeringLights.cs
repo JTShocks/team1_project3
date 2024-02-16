ad996e8f9e112285afc8e863df728bae55dc896a
JTShocks <jacobdreyer2002@gmail.com>
2024-02-16 12:09:18 -0500

    FlickeringLights script finished
    It is very basic, but should work for what we need

--- Assets/Scripts/Fancy/FlickeringLights.cs
+++ Assets/Scripts/Fancy/FlickeringLights.cs
@@ -12,24 +12,50 @@ public class FlickeringLights : MonoBehaviour
     //2. How long the entire interval for the lights is
     //Ex. You set the interval to "2", and the off time to "1", the light will be off for 1 second every 2 second interval
     [SerializeField] float flickeringInterval;
+    [SerializeField] float whenToFlickerTime;
     [SerializeField] float offTime;
+    [SerializeField] int numberOfFlickers;
+
+    Coroutine flickerRoutine;
 
     float lightTimer;
 
-    SpotLight lightToFlicker;
+    Light lightToFlicker;
     // Start is called before the first frame update
     void Awake()
     {
-        lightToFlicker = GetComponent<SpotLight>();
+        lightToFlicker = GetComponent<Light>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
         lightTimer -= Time.deltaTime;
+
         if(lightTimer <= 0)
         {
+            if(flickerRoutine != null)
+            {
+                StopCoroutine(flickerRoutine);
+            }
             //Restart the interval
+            lightTimer = flickeringInterval;
+            flickerRoutine = StartCoroutine(FlickerRoutine());
         }
+    }
+
+    IEnumerator FlickerRoutine()
+    {
+        for(int i = 0; i < numberOfFlickers; i++)
+        {
+            yield return new WaitForSeconds(whenToFlickerTime);
+            lightToFlicker.enabled = false;
+            yield return new WaitForSeconds(offTime);
+            lightToFlicker.enabled = true;
+        }
+
+        
     }
 }
